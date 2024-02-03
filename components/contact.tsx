eabffccdb6fe65b18fa80b098520aef4d344d632
Vijayarun00100 <blockloader78@gmail.com>
2024-02-03 12:43:34 +0530

    responsive 100%

--- components/contact.tsx
+++ components/contact.tsx
@@ -7,25 +7,17 @@ import Linkedin from "@/public/images/linkedin.png";
 import Instagram from "@/public/images/instagram.png"
 export default function contact(){
     return(
-        <div className="bg-gradient-to-r from-blue-900/20 to-blue-500/20 bg-opacity-10 w-screen sm:h-80 h-11/12 flex mt-10 flex-col items-center">
+        <div className="bg-gradient-to-r from-blue-900/20 to-blue-500/20 bg-opacity-10 w-screen sm:h-52 h-11/12 flex mt-10 flex-col items-center">
   <h3 className="text-slate-400 sm:text-4xl mt-9">Get in Touch❕</h3>
   <p className="mt-6 sm:text-2xl text-cyan-500">Feel free to reach out</p>
   
   <div className="flex gap-6 mt-5">
 
     <div className="flex gap-6">
-      <div className="bg-slate-500 w-10 h-10 rounded-full"><img src={Github.src} alt="github" /></div>
-      <div className="bg-slate-500 w-10 h-10 rounded-full"><img src={Gmail.src} alt="gmail" className="w-7 h-7 ml-2 mt-2" /></div>
-      <div className="bg-slate-500 w-10 h-10 rounded-full"><img src={Linkedin.src} alt="linkedin" className="w-7 h-7 ml-1 mt-2" /></div>
-      <div className="bg-slate-500 w-10 h-10 rounded-full"><img src={Instagram.src} alt="Instagram" className="w-7 h-7 ml-2 mt-2" /></div>
-    </div>
-    
-
-    <div className="text-white text-sm">
-      <li><a href="https://github.com/vijayarun00100">Github</a></li>
-      <li><a href="mailto:blockloader78@gmail.com">Gmail</a></li>
-      <li><a href="https://www.linkedin.com/in/vijay-arunachalam/">Linked IN</a></li>
-      <li><a href="https://www.instagram.com/vijayarun_/">Instagram</a></li>
+      <li className="bg-slate-500 w-10 h-10 rounded-full list-none"><a href="https://github.com/vijayarun00100"><img src={Github.src} alt="github" /></a></li>
+      <div className="bg-slate-500 w-10 h-10 rounded-full"><a href="mailto:blockloader78@gmail.com"><img src={Gmail.src} alt="gmail" className="w-8 h-8 ml-1 mt-1" /></a></div>
+      <div className="bg-slate-500 w-10 h-10 rounded-full"><a href="https://www.linkedin.com/in/vijay-arunachalam/"><img src={Linkedin.src} alt="linkedin" className="w-8 h-8 ml-1 mt-1" /></a></div>
+      <div className="bg-slate-500 w-10 h-10 rounded-full"><a href="https://www.instagram.com/vijayarun_/"><img src={Instagram.src} alt="Instagram" className="w-8 h-8 ml-1 mt-1" /></a></div>
     </div>
   </div>
 </div>
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,25 +1,34 @@
 "use client";
 import React from "react";
-import experience from "./experience";
+import Link from "next/link";
 export default function Nav() {
   return (
     <div className="flex flex-col sm:flex-row py-3 list-none backdrop-blur-lg backdrop-opacity-100	backdrop-brightness-150 ">
       <li className="hover:bg-slate-600 w-32 rounded-lg text-center ml-1 sm:ml-2 m-0 p-1 mr-0 text-white opacity-100 mt-1">
-        <p>Portfolio</p>
+        <a href="https://github.com/vijayarun00100">
+          <p>Github</p>
+        </a>
       </li>
       <li className="w-32 hover:bg-slate-600 rounded-lg text-center  mr-3 p-1 ml-0 text-white mt-1">
-        <p> Resume</p>
+        <a href="https://drive.google.com/file/d/10FYT5wPp1WINVP1zxHzuRDF1cDTZVH5u/view?usp=sharing">
+          Resume
+        </a>
       </li>
       <div className=" sm:w-2/3  bg-sky-700 dark:bg-sky-600 rounded-md hover:opacity-100 pointer-events-auto pl-2 ml-0">
         <div className="flex	text-white text-left pl-5 pt-1 mt-1  antialiased font-medium ">
           <h1>Vijay Arunachalam</h1>
           <button className=" hover:bg-sky-400 hover:px-1 hover:pl-2 px-1.5 py-0.5 rounded-md text-center ml-auto mr-2 mb-1	p-2 hover:h-auto">
-            Explore 🌍
+            <Link href="/explore">
+              Explore 🌍
+            </Link>
           </button>
+          
         </div>
       </div>
       <li className="w-32 hover:bg-slate-600 rounded-lg text-center  ml-3 mr-3 p-1 text-white mt-1">
-        <p>Experience</p>
+        <Link href="/experience">
+            <p>Experience</p>
+        </Link>
       </li>
     </div>
   );
